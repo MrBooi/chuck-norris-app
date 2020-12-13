f50860fc4d87b7ebaef51a0b8c5423de383a5cfb
MrBooi <ayabongabooi2@gmail.com>
2020-12-13 22:33:47 +0200

    added chuck joke remote datasource

---
+++ lib/views/chuck_joke/data/datasources/chuck_joke_remote_datasource.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:chuck_norris/core/error/exception.dart';
+import 'package:chuck_norris/views/chuck_joke/domain/entities/chuck_joke.dart';
+
+import 'package:http/http.dart' as http;
+
+abstract class ChuckJokeRemoteDatasource {
+  /// Calls the https://api.chucknorris.io/jokes/random endpoint.
+  ///
+  /// Throws a [ServerException] for all error codes.
+  Future<ChuckJoke> getRandomChuckJoke();
+}
+
+class ChuckJokeRemoteDatasourceImpl implements ChuckJokeRemoteDatasource {
+  final http.Client client;
+
+  ChuckJokeRemoteDatasourceImpl(this.client);
+
+  @override
+  Future<ChuckJoke> getRandomChuckJoke() async {
+    final url = "https://api.chucknorris.io/jokes/random";
+
+    final response = await client.get(url, headers: {
+      'content-Type': 'application/json',
+    });
+
+    if (response.statusCode == 200) {
+      return ChuckJoke.fromJson(
+          json.decode(response.body) as Map<String, dynamic>);
+    } else {
+      throw ServerException();
+    }
+  }
+}
